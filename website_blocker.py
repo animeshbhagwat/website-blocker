7ef931a1cab3d7c98f12aef1022d7ce8a223191f
Animesh <animesh.bhagwat10@gmail.com>
2020-05-12 18:14:44 +0530

    First Commit

---
+++ website_blocker.py
@@ -0,0 +1,29 @@
+import time
+from datetime import datetime as dt
+
+hosts_temp = r'E:\Animesh\Python Projects\App3\hosts'
+# hosts_path = r'C:\Windows\System32\drivers\etc\hosts'
+redirect = '127.0.0.1'
+website_list = ['www.facebook.com', 'www.twitter.com', 'www.instagram.com']
+
+while True:
+    if dt(dt.now().year, dt.now().month, dt.now().day, 9) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 17):
+        print('Working hours')
+        with open(hosts_temp, 'r+') as file:
+            content = file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(f'{redirect} {website} \n')
+    else:
+        print('Fun hours')
+        with open(hosts_temp, 'r+') as file:
+            content = file.readlines()
+            file.seek(0)
+            file.truncate()
+            for line in content:
+                if not any(website in line for website in website_list) :
+                    file.write(line)
+
+    time.sleep(5)
